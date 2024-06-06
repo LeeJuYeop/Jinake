c60acefeb26b9d9bf09eaf2bd6154fc6c4767c92
LeeJuYeop <57062483+LeeJuYeop@users.noreply.github.com>
2024-06-06 15:59:49 +0900

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -34,7 +34,7 @@ namespace snake{
             {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},
             {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},
             {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},
-            {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+','+','+','+','+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},// 가운데
+            {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+','+','#','+','+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},// 가운데
             {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},
             {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},
             {'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'},
@@ -113,71 +113,48 @@ namespace snake{
             {'#','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','+','#'}}
     };
 
-    // 생성자 호출하면 21 39 크기의 gameBoard 만듬
-    Board::Board()
-    {
-        // stdscr의 최대 높이, 최대 너비를 구한다
+    // 생성자 호출하여 21 39 크기의 Board 만들기
+    Board::Board() {
+        gameSpeed = 300;
+
         getmaxyx(stdscr, yMax, xMax);
-        // 21 39 크기의 창을 만들되, 중앙에 생성하도록 한다
         gameBoard = newwin(21, 39, (yMax / 2)-10, (xMax / 2)-19);
         scoreBoard = newwin(10,25, (yMax / 2)-10,(xMax / 2)+25);
         missionBoard = newwin(10, 25, (yMax / 2)+1, (xMax / 2)+25);
-        wtimeout(gameBoard, 300);
+        wtimeout(gameBoard, gameSpeed);
     }
 
     // 초기화 함수
-    void Board::initialize(int num)
-    {
-        // 일단 gameBoard 클리어를 한다
+    void Board::initialize(int num) {
         wclear(gameBoard);
         wclear(scoreBoard);
         wclear(missionBoard);
 
         stageNum = num;
 
-        // stage의 첫번째 맵을 그대로 가져와서 gameBoard 메모리상에 복사한다
         for (int i = 0; i < 21; i++)
         {
-            for (int j = 0; j < 39; j++)
-            {
-                mvwaddch(gameBoard, i, j, stage[stageNum][i][j]);
-            }
+            for (int j = 0; j < 39; j++) { mvwaddch(gameBoard, i, j, stage[stageNum][i][j]); }
         }
     }
 
-    // gameBoard의 메모리상에서 (y, x) 위치에 ch 문자 추가하기
-    void Board::add(int y, int x, char ch)
-    {
-        mvwaddch(gameBoard, y, x, ch);
-    }
+    // Board의 메모리상에서 (y, x) 위치에 ch 문자 추가하기
+    void Board::add(int y, int x, char ch) { mvwaddch(gameBoard, y, x, ch); }
 
-    // gameBoard의 (y, x) 위치에서의 문자 값 get하기
-    char Board::getCharAt(int y, int x)
-    {
-        return mvwinch(gameBoard, y, x);
-    }
+    // Board의 (y, x) 위치의 문자 값 가져오기
+    char Board::getCharAt(int y, int x) { return mvwinch(gameBoard, y, x); }
 
-    // 입력 값 get하기
-    char Board::getInput()
-    {
-        return wgetch(gameBoard);
-    }
+    // 입력 값 가져오기
+    char Board::getInput() { return wgetch(gameBoard); }
 
-    // 현재 게임의 stage 번호 get하기
-    int Board::getStageNum()
-    {
-        return stageNum;
-    }
+    // 현재 게임의 단계 번호 가져오기
+    int Board::getStageNum() { return stageNum; }
 
     // 게임 속도 조절하기
-    void Board::setTimeout(int tick)
-    {
-        wtimeout(gameBoard, tick);
-    }
+    void Board::setTimeout(int tick) { wtimeout(gameBoard, tick); }
 
-    // gameBoard 새로고침
-    void Board::refresh()
-    {   
+    // Board 새로고침 하기
+    void Board::refresh() {   
         box(scoreBoard, 0, 0);
         box(missionBoard, 0, 0);
         wrefresh(gameBoard);
@@ -186,23 +163,26 @@ namespace snake{
     }
 
     // 아이템(사과, 폭탄) 놓을 수 있는 위치 찾기
-    void Board::getItemPos(int &y, int &x)
-    {   
-        // y, x를 각각 0부터 21까지 랜덤 돌려서 gameBoard의 (y, x)가 공백일 때까지 반복
-        // 이때 y, x는 레퍼런스로 받아서 랜덤 돌려질때마다 실질값으로 적용됨
+    void Board::getItemPos(int &y, int &x) {   
         while ((mvwinch(gameBoard, y = rand() % 21, x = rand() % 39)) != ' ');
     }
 
     // 워프 놓을 수 있는 위치 찾기
-    void Board::getWarpPos(int &y, int &x)
-    {
+    void Board::getWarpPos(int &y, int &x) {
         int tmp = rand() % 2;
+
         if (tmp == 0) { while ((mvwinch(gameBoard, y = rand() % 21, x = rand() % 39)) != ' '); }
         else { while ((mvwinch(gameBoard, y = rand() % 21, x = rand() % 39)) != '+'); }
     }
 
-    void Board::recording(int size, int appleS, int bombS, int warpS, 
-        bool sizeC, bool appleC, bool bombC, bool warpC){
+    int Board::getSpeed() { return gameSpeed; }
+    void Board::setSpeed(int S) {
+        wtimeout(gameBoard, gameSpeed);
+        gameSpeed = S;
+    }
+
+    // 스코어 보드와 미션 보드에 점수 기록하기
+    void Board::recording(int size, int appleS, int bombS, int warpS, bool sizeC, bool appleC, bool bombC, bool warpC) {
         mvwprintw(scoreBoard, 1, 3, "~ Score board ~");
         mvwprintw(scoreBoard, 3, 3, "Size : %d ", size);
         mvwprintw(scoreBoard, 4, 3, "Apple : %d ", appleS);
@@ -221,10 +201,8 @@ namespace snake{
         if(warpC){mvwprintw(missionBoard, 6, 3, "Wrap : 3/3 ( v )");}
         else{mvwprintw(missionBoard, 6, 3, "Wrap : %d/3 (  )", warpS);}
     }
-    int Board::getxMax(){
-        return xMax;
-    }
-    int Board::getyMax(){
-        return yMax;
-    }
+
+
+    int Board::getxMax() { return xMax; }
+    int Board::getyMax() { return yMax; }
 }
