8d65ae9be60b717a7be2b1a47fdd9f25aff3302f
LeeJuYeop <57062483+LeeJuYeop@users.noreply.github.com>
2024-06-06 16:00:12 +0900

    Update board.hpp

--- board.hpp
+++ board.hpp
@@ -8,28 +8,28 @@ namespace snake{
     class Board
     {
     public:
-        // 생성자 호출하면 21 21 크기의 gameBoard 만듬
+        // 생성자 호출하여 21 39 크기의 Board 만들기
         Board();
 
         // 초기화 함수
         void initialize(int num);
 
-        // gameBoard의 메모리상에서 (y, x) 위치에 ch 문자 추가하기
+        // Board의 메모리상에서 (y, x) 위치에 ch 문자 추가하기
         void add(int y, int x, char ch);
 
-        // gameBoard의 (y, x) 위치에서의 문자 값 get하기
+        // Board의 (y, x) 위치의 문자 값 가져오기
         char getCharAt(int y, int x);
 
-        // 입력 값 get하기
+        // 입력 값 가져오기
         char getInput();
 
-        // 현재 게임의 stage 번호 get하기
+        // 현재 게임의 단계 번호 가져오기
         int getStageNum();
 
         // 게임 속도 조절하기
         void setTimeout(int tick);
 
-        // gameBoard 새로고침
+        // Board 새로고침 하기
         void refresh();
 
         // 아이템(사과, 폭탄) 놓을 수 있는 위치 찾기
@@ -38,14 +38,18 @@ namespace snake{
         // 워프 놓을 수 있는 위치 찾기
         void getWarpPos(int &y, int &x);
 
-        // 스코어 기록하기
-        void recording(int size, int appleS, int bombS, int warpS, 
-        bool sizeC, bool appleC, bool bombC, bool warpC);
+        // 스코어 보드와 미션 보드에 점수 기록하기
+        void recording(int size, int appleS, int bombS, int warpS, bool sizeC, bool appleC, bool bombC, bool warpC);
+
 
         int getxMax();
         int getyMax();
 
+        int getSpeed();
+        void setSpeed(int S);
+
     private:
+        int gameSpeed;
         int xMax, yMax;
         WINDOW *gameBoard;
         int stageNum;
