68eaf66dee67a960cd044cf4caf50e44457762fe
LeeJuYeop <57062483+LeeJuYeop@users.noreply.github.com>
2024-06-14 22:23:41 +0900

    보드수정

--- board.hpp
+++ board.hpp
@@ -8,40 +8,40 @@ namespace snake{
     class Board
     {
     public:
-        // 생성자 호출하여 21 39 크기의 Board 만들기
+        // 보드 생성자 호출.
         Board();
 
-        // 초기화 함수
-        void initialize(int num);
+        // 스테이지 넘버로 보드 초기화.
+        void initializeBoard(int num);
 
-        // Board의 메모리상에서 (y, x) 위치에 ch 문자 추가하기
-        void add(int y, int x, char ch);
+        // Board 특정 위치에 문자 추가
+        void addChar(int y, int x, char ch);
 
-        // Board의 (y, x) 위치의 문자 값 가져오기
-        char getCharAt(int y, int x);
+        // Board의 특정 위치 문자 불러오기
+        char getChar(int y, int x);
 
-        // 입력 값 가져오기
+        // 입력 값 받기.
         char getInput();
 
-        // 현재 게임의 단계 번호 가져오기
+        // 스테이지 번호 받기.
         int getStageNum();
 
         // 게임 속도 조절하기
         void setTimeout(int tick);
 
-        // Board 새로고침 하기
-        void refresh();
+        // Board를 새로고침
+        void refreshBoard();
 
-        // 아이템(사과, 폭탄) 놓을 수 있는 위치 찾기
-        void getItemPos(int &y, int &x);
+        // item 배치 가능한 좌표 찾기
+        void searchItemPos(int &y, int &x);
 
-        // 워프 놓을 수 있는 위치 찾기
-        void getWarpPos(int &y, int &x);
+        // 워프 배치 가능한 좌표 찾기
+        void searchWarpPos(int &y, int &x);
 
         // 스코어 보드와 미션 보드에 점수 기록하기
         void recording(int size, int appleS, int bombS, int warpS, bool sizeC, bool appleC, bool bombC, bool warpC);
 
-
+        // 내 화면 상의 창의 가로길이, 세로길이 받기.
         int getxMax();
         int getyMax();
 
